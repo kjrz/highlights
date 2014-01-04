dbf1312dd4e0abc83fc51abab31d1a61d6b8b7e4
kjrz <kuba.jarz@gmail.com>
2014-01-04 23:03:20 +0100

    formatting and raising Toasts

--- src/com/github/kjrz/highlights/activity/MainActivity.java
+++ src/com/github/kjrz/highlights/activity/MainActivity.java
@@ -1,33 +1,38 @@
 package com.github.kjrz.highlights.activity;
 
-import com.github.kjrz.highlights.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
+
+import com.github.kjrz.highlights.R;
 
 public class MainActivity extends Activity {
 
-	@Override protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override public boolean onCreateOptionsMenu(Menu menu) {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
+
 		return true;
 	}
 
 	public void startCam(View view) {
 		Intent intent = new Intent(this, CameraActivity.class);
-
 		startActivity(intent);
 	}
 
 	public void startSound(View view) {
-		// TODO Auto-generated method stub
+		// TODO
+
+		Toast.makeText(this, R.string.enter_sound, Toast.LENGTH_SHORT).show();
 	}
 }
