5821c86c6bd8f9e57eb3bb322f412016cf159495
kjrz <kuba.jarz@gmail.com>
2014-01-04 17:29:26 +0100

    renamed camera activity layout

--- src/com/github/kjrz/highlights/CameraActivity.java
+++ src/com/github/kjrz/highlights/CameraActivity.java
@@ -19,7 +19,7 @@ public class CameraActivity extends Activity {
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_cam);
+		setContentView(R.layout.activity_camera);
 
 		// Create an instance of Camera
 		initializeCamera();
