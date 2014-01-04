47bab33778f7163dea4d6d8f1e968e9d8caf8078
kjrz <kuba.jarz@gmail.com>
2014-01-04 15:03:32 +0100

    state of displaying stretched Camera Preview, just before making use of InstantMustache src

---
+++ src/com/github/kjrz/highlights/CameraActivity.java
@@ -0,0 +1,99 @@
+package com.github.kjrz.highlights;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.github.kjrz.highlights.cam.CameraPreview;
+
+public class CameraActivity extends Activity {
+
+	private Camera camera;
+	private CameraPreview cameraPreview;
+
+	private int startStop = R.string.shooter_one;
+
+	@Override protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cam);
+
+		// Create an instance of Camera
+		initializeCamera();
+
+		// Create our Preview view and set it as the content of our activity.
+		cameraPreview = new CameraPreview(this, camera);
+		FrameLayout cameraPreviewFrame = (FrameLayout) findViewById(R.id.camera_preview);
+		cameraPreviewFrame.addView(cameraPreview);
+
+		setupActionBar();
+	}
+
+	private void initializeCamera() {
+		camera = getCameraInstance();
+	}
+
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open();
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	private void setupActionBar() {
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override public boolean onPrepareOptionsMenu(Menu menu) {
+		MenuItem score = menu.findItem(R.id.capture);
+		score.setTitle(startStop);
+		return true;
+	}
+
+	@Override public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.cam, menu);
+		return true;
+	}
+
+	@Override public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override public void onPause() {
+		super.onPause();
+
+		if (camera != null) {
+			camera.release();
+			camera = null;
+		}
+	}
+
+	@Override public void onResume() {
+		super.onResume();
+
+		if (camera == null) {
+			initializeCamera();
+		}
+	}
+
+	public void startStopClicked(MenuItem item) {
+		if (startStop == R.string.shooter_one) {
+			startStop = R.string.shooter_two;
+		} else {
+			startStop = R.string.shooter_one;
+		}
+
+		invalidateOptionsMenu();
+	}
+}
--- src/com/github/kjrz/highlights/MainActivity.java
+++ src/com/github/kjrz/highlights/MainActivity.java
@@ -1,19 +1,31 @@
 package com.github.kjrz.highlights;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void startCam(View view) {
+		Intent intent = new Intent(this, CameraActivity.class);
+
+		startActivity(intent);
+	}
+
+	public void startSound(View view) {
+		// TODO Auto-generated method stub
 	}
 }
